ee96bd2f7f0f6bd3a041ee24b09899176767bb52
Illia <ilya.pon.pl@gmail.com>
2026-03-06 17:24:15 +0100

    Minor fixes

--- Profile-Service/src/main/java/ilya/pon/profile/configuration/security/CustomWebSecurityConfigurerAdapter.java
+++ Profile-Service/src/main/java/ilya/pon/profile/configuration/security/CustomWebSecurityConfigurerAdapter.java
@@ -5,12 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 public class CustomWebSecurityConfigurerAdapter {
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) {
         http
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
--- Profile-Service/src/main/java/ilya/pon/profile/dto/RegisterDto.java
+++ Profile-Service/src/main/java/ilya/pon/profile/dto/RegisterDto.java
@@ -1,7 +1,6 @@
 package ilya.pon.profile.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record RegisterDto (
         @NotBlank String firstName,
